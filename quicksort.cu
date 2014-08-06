923fd2e1e98fd5a9059e777673718abbe94e51d0
Marius Rejdak <mariuswol@gmail.com>
2014-08-06 19:27:13 +0200

    clean up

--- quicksort.cu
+++ quicksort.cu
@@ -1,198 +1,94 @@
 /*
- * quickSort.cu
- *
- * ######## QUICK SORT ########
+ * bitonic_sort.cu
  *
  */
 
- #include <time.h>
- #include <stdio.h>
- #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
 
- #define MAX_THREADS	128
- #define N		513
+// Kernel function
+__global__ static void Quicksort(int* __restrict__ values,
+                                 const uint N)
+{
+#define MAX_LEVELS	300
 
- int*	r_values;
- int*	d_values;
+    int pivot, L, R;
+    int idx =  threadIdx.x + blockIdx.x * blockDim.x;
+    int start[MAX_LEVELS];
+    int end[MAX_LEVELS];
 
- // initialize data set
- void Init(int* values, int i) {
-	srand( time(NULL) );
-        printf("\n------------------------------\n");
+    start[idx] = idx;
+    end[idx] = N - 1;
+    while (idx >= 0) {
+        L = start[idx];
+        R = end[idx];
+        if (L < R) {
+            pivot = values[L];
+            while (L < R) {
+                while (values[R] >= pivot && L < R)
+                    R--;
+                if(L < R)
+                    values[L++] = values[R];
+                while (values[L] < pivot && L < R)
+                    L++;
+                if (L < R)
+                    values[R--] = values[L];
+            }
+            values[L] = pivot;
+            start[idx + 1] = L + 1;
+            end[idx + 1] = end[idx];
+            end[idx++] = L;
+            if (end[idx] - start[idx] > end[idx - 1] - start[idx - 1]) {
+                // swap start[idx] and start[idx-1]
+                int tmp = start[idx];
+                start[idx] = start[idx - 1];
+                start[idx - 1] = tmp;
 
-        if (i == 0) {
-        // Uniform distribution
-                printf("Data set distribution: Uniform\n");
-                for (int x = 0; x < N; ++x) {
-                        values[x] = rand() % 100;
-                        //printf("%d ", values[x]);
-                }
-        }
-        else if (i == 1) {
-        // Gaussian distribution
-        #define MEAN    100
-        #define STD_DEV 5
-                printf("Data set distribution: Gaussian\n");
-                float r;
-                for (int x = 0; x < N; ++x) {
-                        r  = (rand()%3 - 1) + (rand()%3 - 1) + (rand()%3 - 1);
-                        values[x] = int( round(r * STD_DEV + MEAN) );
-                        //printf("%d ", values[x]);
-                }
-        }
-        else if (i == 2) {
-        // Bucket distribution
-                printf("Data set distribution: Bucket\n");
-                int j = 0;
-                for (int x = 0; x < N; ++x, ++j) {
-                        if (j / 20 < 1)
-                                values[x] = rand() % 20;
-                        else if (j / 20 < 2)
-                                values[x] = rand() % 20 + 20;
-                        else if (j / 20 < 3)
-                                values[x] = rand() % 20 + 40;
-                        else if (j / 20 < 4)
-                                values[x] = rand() % 20 + 60;
-                        else if (j / 20 < 5)
-                                values[x] = rand() % 20 + 80;
-                        if (j == 100)
-                                j = 0;
-                        //printf("%d ", values[x]);
-                }
-        }
-        else if (i == 3) {
-        // Sorted distribution
-                printf("Data set distribution: Sorted\n");
-                /*for (int x = 0; x < N; ++x)
-                        printf("%d ", values[x]);
-		*/
-        }
-	else if (i == 4) {
-        // Zero distribution
-                printf("Data set distribution: Zero\n");
-                int r = rand() % 100;
-                for (int x = 0; x < N; ++x) {
-                        values[x] = r;
-                        //printf("%d ", values[x]);
-                }
-        }
-        printf("\n");
+                // swap end[idx] and end[idx-1]
+                tmp = end[idx];
+                end[idx] = end[idx - 1];
+                end[idx - 1] = tmp;
+            }
+
+        } else
+            idx--;
+    }
 }
 
- // Kernel function
- __global__ static void quicksort(int* values) {
- #define MAX_LEVELS	300
+// program main
+int main(int argc, char** argv)
+{
+    void *h_mem, *d_mem;
+    size_t min_size = 1024UL; //1kB
+    size_t max_size = 1024UL*1024UL*256UL; //256MB
 
-	int pivot, L, R;
-	int idx =  threadIdx.x + blockIdx.x * blockDim.x;
-	int start[MAX_LEVELS];
-	int end[MAX_LEVELS];
+    h_mem = malloc(max_size);
+    assert(h_mem != NULL);
+    gpuErrchk(cudaMalloc(&d_mem, max_size));
 
-	start[idx] = idx;
-	end[idx] = N - 1;
-	while (idx >= 0) {
-		L = start[idx];
-		R = end[idx];
-		if (L < R) {
-			pivot = values[L];
-			while (L < R) {
-				while (values[R] >= pivot && L < R)
-					R--;
-				if(L < R)
-					values[L++] = values[R];
-				while (values[L] < pivot && L < R)
-					L++;
-				if (L < R)
-					values[R--] = values[L];
-			}
-			values[L] = pivot;
-			start[idx + 1] = L + 1;
-			end[idx + 1] = end[idx];
-			end[idx++] = L;
-			if (end[idx] - start[idx] > end[idx - 1] - start[idx - 1]) {
-	                        // swap start[idx] and start[idx-1]
-        	                int tmp = start[idx];
-                	        start[idx] = start[idx - 1];
-                        	start[idx - 1] = tmp;
+    srand(time(NULL));
 
-	                        // swap end[idx] and end[idx-1]
-        	                tmp = end[idx];
-                	        end[idx] = end[idx - 1];
-                        	end[idx - 1] = tmp;
-	                }
+    for(size_t size = min_size; size <= max_size; size <<= 1) {
+        size_t N = size/sizeof(int);
+        init_values_int((int*) h_mem, N);
 
-		}
-		else
-			idx--;
-	}
-}
+        copy_to_device_time(d_mem, h_mem, size);
+        cudaDeviceSynchronize();
 
- // program main
- int main(int argc, char **argv) {
-	printf("./quicksort starting with %d numbers...\n", N);
- 	//unsigned int hTimer;
- 	size_t size = N * sizeof(int);
+        Quicksort_C((int*) d_mem, N);
+        gpuErrchk( cudaPeekAtLastError() );
 
- 	// allocate host memory
- 	r_values = (int*)malloc(size);
+        copy_to_host_time(h_mem, d_mem, size);
+        cudaDeviceSynchronize();
 
-	// allocate device memory
-        cudaMalloc((void**)&d_values, size);
+        printf("after %ld %s\n", N, is_int_array_sorted((int*) h_mem, N, false) ? "true":"false");
+    }
 
-	// allocate threads per block
-        const unsigned int cThreadsPerBlock = 128;
+    cudaFree(d_mem);
+    free(h_mem);
 
-	/* Types of data sets to be sorted:
-         *      1. Normal distribution
-         *      2. Gaussian distribution
-         *      3. Bucket distribution
-         *      4. Sorted Distribution
-         *      5. Zero Distribution
-         */
-
-	for (int i = 0; i < 5; ++i) {
-                // initialize data set
-                Init(r_values, i);
-
-	 	// copy data to device
-		cudaMemcpy(d_values, r_values, size, cudaMemcpyHostToDevice);
-
-		printf("Beginning kernel execution...\n");
-
- 		cudaThreadSynchronize();
-
-		// execute kernel
- 		quicksort <<< MAX_THREADS / cThreadsPerBlock, MAX_THREADS / cThreadsPerBlock, cThreadsPerBlock >>> (d_values);
-
- 		cudaThreadSynchronize();
-
-	 	// copy data back to host
-		cudaMemcpy(r_values, d_values, size, cudaMemcpyDeviceToHost);
-
-	 	// test print
- 		/*for (int i = 0; i < N; i++) {
- 			printf("%d ", r_values[i]);
- 		}
- 		printf("\n");
-		*/
-
-		// test
-                printf("\nTesting results...\n");
-                for (int x = 0; x < N - 1; x++) {
-                        if (r_values[x] > r_values[x + 1]) {
-                                printf("Sorting failed.\n");
-                                break;
-                        }
-                        else
-                                if (x == N - 2)
-                                        printf("SORTING SUCCESSFUL\n");
-                }
-
-	}
-
- 	// free memory
-	cudaFree(d_values);
- 	free(r_values);
-
- 	cudaThreadExit();
+    return 0;
 }
