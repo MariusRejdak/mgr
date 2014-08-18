4478242d92b7beb6d6f7a8b56e61bd0c06d85d05
Marius Rejdak <mariuswol@gmail.com>
2014-08-18 23:40:30 +0200

    Shared merge fix

--- mergeSort.cu
+++ mergeSort.cu
@@ -34,10 +34,10 @@ __global__ static void CUDA_MergeSortSmall(Element* __restrict__ values,
         while (a + b < srcMergeSize) {
             if (b >= srcMergeSize || (a < srcMergeSize && v_a.k < v_b.k)) {
                 shared_out[a + b] = v_a;
-                v_a = shared_a[++a];
+                if (++a < srcMergeSize) v_a = shared_a[a];
             } else {
                 shared_out[a + b] = v_b;
-                v_b = shared_b[++b];
+                if (++b < srcMergeSize) v_b = shared_b[b];
             }
         }
     } else if (TID == TDIM-1) {
@@ -49,10 +49,10 @@ __global__ static void CUDA_MergeSortSmall(Element* __restrict__ values,
         while (a + b + 1 >= srcMergeSize) {
             if (b < 0 || (a >= 0 && v_a.k >= v_b.k)) {
                 shared_out[a + b + 1] = v_a;
-                v_a = --a > 0 ? shared_a[a] : v_a;
+                if (--a > 0) v_a = shared_a[a];
             } else {
                 shared_out[a + b + 1] = v_b;
-                v_b = --b > 0 ? shared_b[b] : v_b;
+                if (--b > 0) v_b = shared_b[b];
             }
         }
     }
@@ -66,18 +66,17 @@ __global__ static void CUDA_MergeSortShared(Element* __restrict__ values,
                                             const int32_t iteration)
 {
     extern __shared__ Element shared_values[];
-    int32_t idx = TDIM * BID + TID;
-
     const int32_t srcMergeSize = 1 << iteration;
-    values += 2*(idx & ~(srcMergeSize - 1));
+    int32_t idx = TDIM * BID + TID;
+    Element* shared_a = shared_values;
+    Element* shared_b = shared_values + srcMergeSize;
+    Element* shared_out = shared_values + (srcMergeSize << 1);
+
+    values += (idx & ~(srcMergeSize - 1)) << 1;
     idx &= srcMergeSize - 1;
 
-    Element* shared_a = shared_values;
-    Element* shared_b = shared_values + (1 << iteration);
-    Element* shared_out = shared_values + (2 << iteration);
-
     shared_a[TID] = values[idx];
-    shared_b[TID] = values[idx + (1 << iteration)];
+    shared_b[TID] = values[idx + srcMergeSize];
     __syncthreads();
 
     if ((TID & MERGE_SIZE - 1) == 0) {
@@ -112,8 +111,8 @@ __global__ static void CUDA_MergeSortShared(Element* __restrict__ values,
     }
 
     __syncthreads();
-    values[idx << 1] = shared_out[TID << 1];
-    values[(idx << 1) + 1] = shared_out[(TID << 1) + 1];
+    values[idx] = shared_out[TID];
+    values[idx + srcMergeSize] = shared_out[TID + srcMergeSize];
 }
 
 __global__ static void CUDA_MergeSortGlobal(Element* __restrict__ values,
@@ -121,21 +120,31 @@ __global__ static void CUDA_MergeSortGlobal(Element* __restrict__ values,
                                             const int32_t iteration,
                                             const int32_t N)
 {
-    //extern __shared__ Element shared_values[];
     const int32_t srcMergeSize = 1 << iteration;
     int32_t idx = TDIM * BID + TID;
+    const int32_t idl = idx & MERGE_SIZE - 1;
+    {
+        const int32_t offset = (idx & ~(srcMergeSize - 1)) << 1;
+        values += offset;
+        values_sorted += offset;
+    }
+    idx &= srcMergeSize - 1;
 
     Element* shared_a = values + 2*(idx & ~(srcMergeSize - 1));
     Element* shared_b = shared_a + srcMergeSize;
+    //__shared__ Element* shared_a[MERGE_SIZE]; // = values + 2*(idx & ~(srcMergeSize - 1));
+    //__shared__ Element* shared_b[MERGE_SIZE]; // = shared_a + srcMergeSize;
+    //__shared__ Element* shared_out[MERGE_SIZE << 1]; // = values_sorted + 2*(idx & ~(srcMergeSize - 1));
     Element* shared_out = values_sorted + 2*(idx & ~(srcMergeSize - 1));
-    idx &= srcMergeSize - 1;
 
     int32_t a = idx & ~(MERGE_SIZE - 1);
     int32_t a_end = a + MERGE_SIZE;
     int32_t b = a;
     int32_t b_end = a_end;
 
-    if ((idx & MERGE_SIZE - 1) == 0) {
+    //shared_a[idl] = values[]
+
+    if (idl == 0) {
         if (a > 0) {
             const Key a_min = shared_a[a].k;
             while (b > 0 && a_min <= shared_b[b].k) b -= MERGE_SIZE;
@@ -175,7 +184,7 @@ __host__ void inline MergeSort(Element** d_mem_values,
             CUDA_MergeSortShared<<<v.dim_blocks, v.num_threads, v.num_threads*sizeof(Element) << 2>>>(*d_mem_values, i);
         }
         else {
-            kdim v = get_kdim(N/2);
+            kdim v = get_kdim_nt(N/2, MERGE_SIZE);
             CUDA_MergeSortGlobal<<<v.dim_blocks, v.num_threads>>>(*d_mem_values, *d_mem_sorted, i, N);
             swap((void**)d_mem_values, (void**)d_mem_sorted);
         }
